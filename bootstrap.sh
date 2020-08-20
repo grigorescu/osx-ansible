a0f4287effda4925f36d1f8bea4db7f6877304bf
Vlad Grigorescu <grigorescu@gmail.com>
2020-08-19 20:49:41 -0500

    Refactor bootstrap script a bit

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,11 @@
 
 # shell script to bootstrap the system to the point that it has ansible
 if [ ! -d /Library/Developer ]; then
-    echo "Insalling Xcode first, then rerun this script"
+    echo "Installing Xcode first..."
     sleep 1
     xcode-select --install
-else
-   /bin/bash ./roles/homebrew/files/install-homebrew.sh
-   type ansible >/dev/null 2>&1 || /usr/local/bin/brew install ansible
 fi
+
+type brew >/dev/null 2>&1 || /bin/bash ./roles/homebrew/files/install-homebrew.sh
+
+type ansible >/dev/null 2>&1 || /usr/local/bin/brew install ansible
