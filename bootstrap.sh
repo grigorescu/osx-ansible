13fa6600d707cf1814ced83f81602a59c42ddd01
Vlad Grigorescu <grigorescu@gmail.com>
2020-08-19 20:53:19 -0500

    Install ansible-lint

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,16 @@
 # shell script to bootstrap the system to the point that it has ansible
 if [ ! -d /Library/Developer ]; then
     echo "Installing Xcode first..."
-    sleep 1
     xcode-select --install
 fi
+
+while [ ! -d /Library/Developer ]; do
+    sleep 10
+    echo "Waiting on XCode install..."
+done
 
 type brew >/dev/null 2>&1 || /bin/bash ./roles/homebrew/files/install-homebrew.sh
 
 type ansible >/dev/null 2>&1 || /usr/local/bin/brew install ansible
+
+type ansible-lint >/dev/null 2>&1 || pip install ansible-lint
